28c5ad48248f372302bce21866707ce8d93470c8
Messadi Said Abdesslem <s.messadi@esi-sba.dz>
2021-08-12 17:08:01 +0100

    added backend for add qty mince qty remove item from cart

--- controllers/panierController.js
+++ controllers/panierController.js
@@ -2,9 +2,10 @@ const Product = require("../models/productModel");
 const Cart = require("../models/cart");
 const { promisify } = require("util");
 const jwt = require("jsonwebtoken");
+const catchAsync = require("../utils/catchAsync");
 const AppError = require("../utils/appError");
 
-exports.getProduct = async (req, res) => {
+exports.getProduct = catchAsync(async (req, res) => {
   const productId = req.params.id;
 
   try {
@@ -73,9 +74,9 @@ exports.getProduct = async (req, res) => {
     console.log(err.message);
     res.redirect("/");
   }
-};
+});
 
-exports.getPanier = async (req, res) => {
+exports.getPanier = catchAsync(async (req, res) => {
   try {
     const decoded = await promisify(jwt.verify)(
       req.cookies.jwt,
@@ -114,7 +115,7 @@ exports.getPanier = async (req, res) => {
     console.log(err.message);
     res.redirect("/");
   }
-};
+});
 
 async function productsFromCart(cart) {
   let products = [];
@@ -127,3 +128,197 @@ async function productsFromCart(cart) {
   }
   return products;
 }
+
+exports.addOneToQuantity = catchAsync(async (req, res) => {
+  // post req
+  // req.body.productId
+  const productId = req.body.productId;
+  try {
+    const decoded = await promisify(jwt.verify)(
+      req.cookies.jwt,
+      process.env.JWT_SECRET
+    );
+    // get the correct cart, either from the db, session, or an empty cart.
+    let user_cart;
+
+    if (req.cookies.jwt) {
+      user_cart = await Cart.findOne({ user: decoded._id });
+    }
+    let cart;
+
+    if (req.cookies.jwt && !user_cart) {
+      cart = new Cart({});
+    } else if (req.cookies.jwt && user_cart) {
+      cart = user_cart;
+    } else {
+    }
+
+    // add the product to the cart
+    const product = await Product.findById(productId);
+    if (!product) {
+      return res
+        .status(400)
+        .json({
+          status: "failed",
+          error: "no suck product with the provided id",
+        });
+    }
+    const itemIndex = cart.items.findIndex((p) => p.productId == productId);
+
+    if (itemIndex > -1) {
+      // if product exists in the cart, update the quantity
+      cart.items[itemIndex].qty++;
+      cart.items[itemIndex].price = cart.items[itemIndex].qty * product.price;
+      cart.totalQty++;
+      cart.totalCost += product.price;
+    } else {
+      // if product does not exists in cart, find it in the db to retrieve its price and add new item
+      cart.items.push({
+        productId: productId,
+        qty: 1,
+        price: product.price,
+        title: product.name,
+      });
+      cart.totalQty++;
+      cart.totalCost += product.price;
+    }
+    //if the user is logged in, store the user's id and save cart to the db
+    if (req.cookies.jwt) {
+      cart.user = decoded._id;
+      await cart.save();
+    }
+
+    /*    req.session.cart = cart;
+    req.flash("success", "Item added to the shopping cart");
+    res.redirect(req.headers.referer);*/
+    res.status(200).json({
+      status: "success",
+      cart,
+    });
+  } catch (err) {
+    console.log(err.message);
+    res.status(400).json({ status: "failed", error: err.message });
+  }
+});
+
+exports.TakeOneFromQuantity = catchAsync(async (req, res) => {
+  // post req
+  // req.body.productId
+  const productId = req.body.productId;
+  try {
+    const decoded = await promisify(jwt.verify)(
+      req.cookies.jwt,
+      process.env.JWT_SECRET
+    );
+    // get the correct cart, either from the db, session, or an empty cart.
+    let user_cart;
+
+    if (req.cookies.jwt) {
+      user_cart = await Cart.findOne({ user: decoded._id });
+    }
+    let cart;
+
+    if (req.cookies.jwt && !user_cart) {
+      cart = new Cart({});
+    } else if (req.cookies.jwt && user_cart) {
+      cart = user_cart;
+    } else {
+    }
+
+    // add the product to the cart
+    const product = await Product.findById(productId);
+
+    const itemIndex = cart.items.findIndex((p) => p.productId == productId);
+
+    if (itemIndex > -1) {
+      // if there is only one item to delete
+      if (cart.items[itemIndex].qty == 1) {
+        cart.totalQty--;
+        cart.totalCost -= product.price;
+        cart.items.splice(itemIndex, 1);
+      } else {
+        cart.items[itemIndex].qty--;
+        cart.items[itemIndex].price = cart.items[itemIndex].qty * product.price;
+        cart.totalQty--;
+        cart.totalCost -= product.price;
+      }
+    } else {
+      return res.status(404).json({
+        status: "failed",
+        error: "no such item in the cart",
+      });
+    }
+    //if the user is logged in, store the user's id and save cart to the db
+    if (req.cookies.jwt) {
+      cart.user = decoded._id;
+      await cart.save();
+    }
+
+    res.status(200).json({
+      status: "success",
+      cart,
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "failed",
+      error: err.message,
+    });
+  }
+});
+
+exports.removeItemFromCart = catchAsync(async (req, res) => {
+  // post req
+  // req.body.productId
+  const productId = req.body.productId;
+  try {
+    const decoded = await promisify(jwt.verify)(
+      req.cookies.jwt,
+      process.env.JWT_SECRET
+    );
+    // get the correct cart, either from the db, session, or an empty cart.
+    let user_cart;
+
+    if (req.cookies.jwt) {
+      user_cart = await Cart.findOne({ user: decoded._id });
+    }
+    let cart;
+
+    if (req.cookies.jwt && !user_cart) {
+      cart = new Cart({});
+    } else if (req.cookies.jwt && user_cart) {
+      cart = user_cart;
+    } else {
+    }
+
+    // add the product to the cart
+    const product = await Product.findById(productId);
+
+    const itemIndex = cart.items.findIndex((p) => p.productId == productId);
+
+    if (itemIndex > -1) {
+      cart.totalQty -= cart.items[itemIndex].qty;
+      cart.totalCost -= cart.items[itemIndex].qty * product.price;
+      cart.items.splice(itemIndex, 1);
+    } else {
+      return res.status(404).json({
+        status: "failed",
+        error: "no such item in the cart",
+      });
+    }
+    //if the user is logged in, store the user's id and save cart to the db
+    if (req.cookies.jwt) {
+      cart.user = decoded._id;
+      await cart.save();
+    }
+
+    res.status(200).json({
+      status: "success",
+      cart,
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "failed",
+      error: err.message,
+    });
+  }
+});
--- routes/panierRouter.js
+++ routes/panierRouter.js
@@ -6,6 +6,19 @@ const router = express.Router();
 router
   .route("/add-to-cart/:id")
   .get(authController.protect, panierController.getProduct);
+
+router
+  .route("/add-quantity")
+  .post(authController.protect, panierController.addOneToQuantity);
+
+router
+  .route("/mince-quantity")
+  .post(authController.protect, panierController.TakeOneFromQuantity);
+
+router
+  .route("/remove-item")
+  .post(authController.protect, panierController.removeItemFromCart);
+
 router.get("/panier", authController.protect, panierController.getPanier);
 
 module.exports = router;
